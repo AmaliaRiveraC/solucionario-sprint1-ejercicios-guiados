bf61e49b8b9198e8f60d2f85fbe5a42a42a1d363
Emmanuel Orozco <yo@emmanuelorozco.com>
2017-10-04 11:56:48 -0500

    added binary search

--- 3-sort/solution.js
+++ 3-sort/solution.js
@@ -19,6 +19,33 @@ function getTheSmallestIndexOfNumberInArray(array) {
 }
 
 var arrayWithData = [6, 5, 4, 3, 2, 1];
-var itemToSearch = 1;
+var itemToSearch = 6;
 
-console.log(selectionSort(arrayWithData));
+arrayWithData = selectionSort(arrayWithData);
+
+console.log("sorted array: " + arrayWithData);
+
+function binarySearch(array, itemToSearch) {
+  var searching = true;
+
+  while (searching == true) {
+    var nIndex = Math.floor(array.length / 2);
+    var n = array[nIndex];
+
+    if (n == itemToSearch) {
+      return true;
+    } else {
+      if (itemToSearch > n) {
+        var array = array.splice(nIndex);
+      } else if (itemToSearch < n) {
+        var array = array.splice(0, nIndex);
+      }
+
+      if (array.length === 1) {
+        return false;
+      }
+    }
+  }
+}
+
+console.log(binarySearch(arrayWithData, itemToSearch));
